21e119ba47c33f305d02fdcfebf0260c73c3e35c
xkz <1060821554@qq.com>
2019-06-09 14:36:18 +0800

    init createAsset

---
+++ bundler.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const babylon = require('babylon')
+const traverse = require('babel-traverse').default
+
+let ID = 0
+
+function createAsset (filename) {
+  const content = fs.readFileSync(filename, 'utf-8')
+
+  const ast = babylon.parse(content, {
+    sourceType: 'module'
+  })
+
+  const dependencies = []
+
+  traverse(ast, {
+    ImportDeclaration: ({ node }) => {
+      dependencies.push(node.source.value)
+    }
+  })
+
+  const id = ID++
+
+  return {
+    id,
+    filename,
+    dependencies
+  }
+}
+
+function createGraph () {
+
+}
+
+const mainAsset = createAsset('./example/entry.js')
+console.log(mainAsset)
